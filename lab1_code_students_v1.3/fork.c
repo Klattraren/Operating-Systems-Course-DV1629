ce0eb25719f9c0fbc877e485b1f8e1f1368f1b51
Tobias Mattsson <tomt21@student.bth.se>
2023-11-13 09:41:02 +0100

    task fork commented

--- lab1_code_students_v1.3/fork.c
+++ lab1_code_students_v1.3/fork.c
@@ -4,28 +4,28 @@
 int main(int argc, char **argv)
 {
     pid_t pid;
-    pid_t pid2;
+    pid_t pid2; //Adding a new PID
     unsigned i;
     unsigned niterations = 100;
-    pid = fork();
-    
-    if (pid == 0) {
+    pid = fork(); //Starting the first child process
+
+    if (pid == 0) { //cheacking if the process is a child process
         for (i = 0; i < niterations; ++i)
             printf("A = %d, ", i);
-     } 
+     }
     else {
-        pid2 = fork();
+        pid2 = fork(); //Starting the second child from the main process
 
-        if (pid2 == 0) {
+        if (pid2 == 0) { //Checking for the second child process
             for (i = 0; i < niterations; ++i)
                 printf("C = %d, ", i);
         }
-        else {
+        else { //If the process is not a child process then it is the main process and here we print the PID's of the child processes and also all B=0, B=1, B=2, ... B=99
             for (i = 0; i < niterations; ++i)
                 printf("B = %d, ", i);
-            
-            printf("\nThis is the childprocess A and it has process ID: %d\n", pid);
-            printf("This is the childprocess C and it has process ID: %d\n", pid2);
+
+            printf("\nThis is the childprocess A and it has process ID: %d\n", pid); //Revealing the PID
+            printf("This is the childprocess C and it has process ID: %d\n", pid2); //Revealing the PID
         }
     }
     printf("\n");
